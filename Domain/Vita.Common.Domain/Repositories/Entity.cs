91db7f8f718dd3072bad301fcd8ad968116f50fd
Diego Arranz García <diego.arranz.garcia@hotmail.com>
2020-05-30 10:32:03 +0200

    Moved Abstrations proyects from Vita.Api to Vita.Common

---
+++ Domain/Vita.Common.Domain/Repositories/Entity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Vita.Domain.Abstractions.Repositories
+{
+
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public bool IsTransient()
+        {
+            return Id == default;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Entity))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            Entity item = (Entity)obj;
+
+            if (item.IsTransient() || IsTransient())
+                return false;
+
+            return item.Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^ 31;
+        }
+    }
+}
---
+++ Domain/Vita.Common.Domain/Repositories/IRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Vita.Domain.Abstractions.Repositories
+{
+    public interface IRepository<TEntity> : IAggregateRoot where TEntity : Entity
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+}
