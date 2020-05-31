cb8292a8bf24e47a3c0458e32ffb7a8cd5e6f253
Diego Arranz García <diego.arranz.garcia@hotmail.com>
2020-05-31 10:23:40 +0200

    Refactored Pagination package

---
+++ Vita.Core.Pagination.Http/Headers/PaginationHeadersExtenstion.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Vita.Core.Http.Pagination.Pagination;
+
+namespace Vita.Core.Pagination.Http.Headers
+{
+	public static class PaginationExtension
+	{
+		public const string DefaultPaginationCookieName = "X-Pagination";
+
+		public static void AddPaginationMetadata<T>(this HttpResponse response, PagedList<T> page, string paginationCookieName = DefaultPaginationCookieName)
+		{
+			var metadata = new PaginationMetadata()
+			{
+				TotalCount = page.TotalCount,
+				PageSize = page.PageSize,
+				CurrentPage = page.CurrentPage,
+				TotalPages = page.TotalPages,
+				HasNext = page.HasNext,
+				HasPrevious = page.HasPrevious
+			};
+
+			response.Headers.Add(paginationCookieName, JsonConvert.SerializeObject(metadata));
+		}
+	}
+}
