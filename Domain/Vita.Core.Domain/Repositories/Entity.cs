5addb6eb5fb95b84c10ab4bb900dae19ab3706b1
Diego Arranz García <diego.arranz.garcia@hotmail.com>
2021-12-24 23:30:09 +0100

    Added accessors to Events

--- Domain/Vita.Core.Domain/Repositories/Entity.cs
+++ Domain/Vita.Core.Domain/Repositories/Entity.cs
@@ -7,8 +7,7 @@ namespace Vita.Core.Domain.Repositories
     public abstract class Entity
     {
         public Guid Id { get; set; }
-
-        public List<DomainEvent> Events = new();
+        public List<DomainEvent> Events { get; set; } = new();
 
         public bool IsTransient()
         {
