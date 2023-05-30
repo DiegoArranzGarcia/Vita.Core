aa566c632311ab1d6263e3a3af1210d68994815d
Diego Arranz García <diego.arranz.garcia@outlook.com>
2023-05-30 15:18:54 +0200

    feature: added new package for common uses

---
+++ Vita.Core/DateTimeInterval.cs
@@ -0,0 +1,16 @@
+﻿namespace Vita.Common;
+
+public readonly record struct DateTimeInterval
+{
+    public DateTimeOffset Start { get; init; }
+    public DateTimeOffset End { get; init; }
+
+    public DateTimeInterval(DateTimeOffset start, DateTimeOffset end)
+    {
+        if (end < start)
+            throw new ArgumentOutOfRangeException(nameof(end));
+
+        Start = start;
+        End = end;
+    }
+}
